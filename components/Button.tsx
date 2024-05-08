a531c6543ba3f95cad7d01b4a782c26db0a1a6df
ProgrammerSiam <programmersiam.com@gmail.com>
2024-05-08 17:35:16 +0600

    fast commit

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+// Define the props type for the Button component
+type ButtonProps = {
+  type: 'button' | 'submit';
+  title: string;
+  icon?: string;
+  variant: string;
+  full?: boolean;
+}
+// Button component definition
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+  return (
+    <button
+    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+    </button>
+  )
+}
+
+export default Button
---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
+import Button from "./Button"
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      {/* Logo */}
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+{/* Navigation links container */}
+      <ul className="hidden h-full gap-12 lg:flex">
+      {/* // Mapping through navigation links */}
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+{/* Login button container */}
+      <div className="lg:flexCenter hidden">
+        <Button 
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+{/* Menu icon */}
+      <Image 
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  )
+}
+
+export default Navbar
